26a5bdca3de755fb5dc60f298643cb435c427bc1
Kim Trong <kimtrongdev@gmail.com>
2023-03-24 13:18:56 +0700

    init default mongo path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const app = express();
 const logger = require('morgan');
@@ -19,13 +20,17 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let monoPath = `mongodb+srv://kimtrongdev2:HUYyfu1ovSqkxJde@cluster0.vawtbzy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+if (process.env.MONGO_URL) {
+    monoPath = `mongodb://${process.env.MONGO_URL || 'localhost:27017'}/${process.env.MONGO_NAME || 'wl-test'}`
+}
 db.mongoose
-    .connect(`mongodb://${process.env.MONGO_URL || 'localhost:27017'}/${process.env.MONGO_NAME || 'wl-test'}`, {
+    .connect(monoPath, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => {
-        console.log("Đã kết nối tới Mongodb Wine Lux.");
+        console.log("Đã kết nối tới Mongodb.");
     })
     .catch(err => {
         console.error("Connection error", err);
@@ -48,6 +53,10 @@ app.use('*', (req, res) => {
 const PORT = process.env.PORT || 8000;
 app.use(logger('dev'));
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    let adminDefaultUser = await db.admin.findOne({ email: 'test@gmail.com' })
+    if (!adminDefaultUser) {
+        db.admin.create({ fullname: 'test', role: 'admin', email: 'test@gmail.com', password: bcrypt.hashSync('123456789', 8) })
+    }
     console.log(`Server is running on port ${PORT}.`);
 });
