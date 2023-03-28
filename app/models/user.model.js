d0bca9581f47aef0606367804fe3f37dc5b2765c
Kim Trong <kimtrongdev@gmail.com>
2023-03-28 21:46:50 +0700

    update user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,23 +1,31 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
+const schema = new mongoose.Schema({
+    fullname: { type: String, default: null },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, default: null },
+    address: {
+        home: { type: String, default: null },
+        ward: { type: String, default: null },
+        district: { type: String, default: null },
+        city: { type: String, default: null },
+    },
+    role: { type: String, default: 'user' },
+    verified: { type: Boolean, default: false },
+    status: { type: Boolean, default: true },
+    created: { type: Date, default: Date.now },
+})
+
+schema.set('timestamps', true);
+schema.statics.ROLES = {
+    ADMIN: 'admin',
+    LEADER: 'leader',
+    EMPLOYEE: 'employee',
+}
+const model = mongoose.model(
     "user",
-    new mongoose.Schema({
-        fullname: { type: String, default: null },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        phone: { type: String, default: null },
-        address: {
-            home: { type: String, default: null },
-            ward: { type: String, default: null },
-            district: { type: String, default: null },
-            city: { type: String, default: null },
-        },
-        role: { type: String, default: 'user' },
-        verified: { type: Boolean, default: false },
-        status: { type: Boolean, default: true },
-        created: { type: Date, default: Date.now },
-    })
+    schema
 );
 
-module.exports = User;
+module.exports = model;
